65ae1d1dbd407be6cdf836f78697c358e62ac341
RicardoMartinez <ricardoemartine@hotmail.com>
2021-03-24 17:40:05 +0100

    Add new tabs to navbar changed avatarand add migration for field in user db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Clearing database"
+
+User.destroy_all
+
+puts "Generating Users"
+
+user_1 = User.new(email: "ricardoemartine@gmail.com", password: "123456", role: "Admin", first_name: "Ricardo", last_name: "Martinez", location: "Amsterdam")
+user_1.save!
+
+user_2 = User.create!(email: "thomas@gmail.com", password: "123456", role: "Tour Guide", first_name: "Thomas", last_name: "Martinez", location: "Amsterdam")
+user_3 = User.create!(email: "martine@gmail.com", password: "123456", role: "user", first_name: "Martine", last_name: "Martinez", location: "Amsterdam")
+user_4 = User.create!(email: "nathan@gmail.com", password: "123456", role: "user", first_name: "Nathan", last_name: "Martinez", location: "Amsterdam")
+
+
+puts "Users created"
