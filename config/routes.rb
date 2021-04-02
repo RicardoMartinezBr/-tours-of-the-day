413173ad5e8376c8046519d22f0d8e0d8c0518b1
RicardoMartinez <ricardoemartine@hotmail.com>
2021-04-02 15:59:01 +0200

    updated seeds and fixed responsiviness issues on tours show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'pages#home'
+
   get 'profile' => 'user#profile'
   get 'settings' => 'users#edit'
-  resources :users do
-    member do
-      get :confirm_email
-    end
-  end
 
-  root to: 'pages#home'
+
   get '/dashboard', to: 'pages#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
