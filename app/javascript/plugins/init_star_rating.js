a3df6fafebe7f0855932a20cb983334c736df215
RicardoMartinez <ricardoemartine@hotmail.com>
2021-04-03 09:23:37 +0200

    fixed start rating issues

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -2,8 +2,12 @@ import "jquery-bar-rating";
 import $ from 'jquery';
 
 const initStarRating = () => {
-  $('#review_tour_rating').barrating({
-    theme: 'css-stars'
+  $('#review_rating').barrating({
+    theme: 'css-stars',
+    onSelect: (value, text, event) => {
+      const form = $("form.review_form"); // We are selecting the form on the page with its class
+      form.submit(); // We submit the form with javascript
+    }
   });
 };
 
