0538ec2e8901450cac166e8d56d060cc769446b8
RicardoMartinez <ricardoemartine@hotmail.com>
2021-04-03 23:30:59 +0200

    fixed UX for Tour givers and normal users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,10 +7,11 @@ class PagesController < ApplicationController
   def dashboard
     # @reservations = policy_scope(Reservation).where(user: current_user).order(created_at: :desc)
     @requests = Reservation.joins(:tour).where(tour: { user: current_user })
-    @user = current_user
+    # @user = current_user
     # @tour.user = @user
     @favorites = current_user.favorites
     @reservation = Reservation.new
+    # @reservations = Reservation.all
     # @chatroom = Chatroom.find_or_create_by(portfolio: @portfolio, user: current_user)
   end
 end
