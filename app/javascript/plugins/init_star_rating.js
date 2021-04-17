aae76764f9be06ef9e4c555c4790e18cd0cc0fd6
RicardoMartinez <ricardoemartine@hotmail.com>
2021-04-17 22:08:48 +0200

    implemented jquery barrating

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -2,7 +2,7 @@ import "jquery-bar-rating";
 import $ from 'jquery';
 
 const initStarRating = () => {
-  $('#review_rating').barrating({
+  $('#review_tour_rating').barrating({
     theme: 'css-stars',
     onSelect: (value, text, event) => {
       const form = $("form.review_form"); // We are selecting the form on the page with its class
